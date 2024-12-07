d425b4090f1e45ad900499cd500881be058a15dd
Muhammad Henry Aditya <adityahenry2@gmail.com>
2024-12-07 12:59:26 +0800

    Fix: fix count after checkout

--- src/plugins/cartCountStore.js
+++ src/plugins/cartCountStore.js
@@ -4,5 +4,7 @@ export default function refreshCount(dispatch) {
     if (localStorage.getItem('cart')) {
         let countCart = JSON.parse(localStorage.getItem('cart')).length
         dispatch(setCount(countCart))    
+    } else {
+        dispatch(setCount(0))    
     }
 }
